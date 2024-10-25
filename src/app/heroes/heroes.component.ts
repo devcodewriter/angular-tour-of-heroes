414188d39394f2e1b707b4976588db827f114114
devcodewriter <acodewriter.dev@gmail.com>
2024-10-25 18:10:11 +0000

    Showing messages.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -15,11 +16,11 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
   selectedHero ?: Hero;
 
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService, private messageService: MessageService){}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -32,5 +33,6 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
